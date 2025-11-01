ef8283d4c6c9811fbc99a6181054674dc2c48f57
Deep Singnapure <deepsingnapure5@gmail.com>
2025-11-01 19:09:46 +0530

    Time: 23 ms (18.48%) | Memory: 177.9 MB (5.52%) - LeetSync

---
+++ 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+import java.util.HashSet;
+class Solution {
+    public ListNode modifiedList(int[] nums, ListNode head) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int num : nums){
+            set.add(num);
+        }
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode curr=head;
+        ListNode prev=dummy;
+        int i=0;
+        while(curr != null){
+           
+           if(set.contains(curr.val)){
+            prev.next=curr.next;
+           }
+           else{
+            prev=curr;
+           }
+           curr=curr.next;
+
+        }
+        return dummy.next;
+    }
+}
