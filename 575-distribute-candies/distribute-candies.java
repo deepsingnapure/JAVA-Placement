9c7a4a56a8c838f761dc75d33cdf5a6eca95f692
Deep Singnapure <deepsingnapure5@gmail.com>
2025-12-25 22:06:50 +0530

    Time: 31 ms (53.88%) | Memory: 48.9 MB (11.69%) - LeetSync

---
+++ 575-distribute-candies/distribute-candies.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int distributeCandies(int[] candyType) {
+        Set<Integer> set = new HashSet<>();
+
+        for(int i:candyType)
+        {
+            set.add(i);
+        }
+
+        int n=candyType.length/2;
+
+        if(set.size() <= n)
+        {
+            return set.size();
+        }
+        return n;
+    }
+}
