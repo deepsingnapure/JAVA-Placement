ad8d782c29d6b9e9d87b34f402cc587d2682dfe4
Deep Singnapure <deepsingnapure5@gmail.com>
2026-01-17 13:33:36 +0530

    Time: 1 ms (38.55%) | Memory: 44.2 MB (99.62%) - LeetSync

---
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -0,0 +1,17 @@
+class Solution {
+    private int solve(int[] cost, int n, int[] dp){
+        if(n == 0) return cost[0];
+        if(n==1) return cost[1];
+
+        if(dp[n] != -1) return dp[n];
+
+        dp[n] = cost[n] + Math.min(solve(cost,n-1,dp),solve(cost,n-2,dp));
+        return dp[n];
+    }
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp,-1);
+        return Math.min(solve(cost,n-1,dp),solve(cost,n-2,dp));
+    }
+}
