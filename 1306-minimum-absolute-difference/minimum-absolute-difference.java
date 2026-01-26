415cb7843c0be4fce98fc44d991f1b9ee47909f1
Deep Singnapure <deepsingnapure5@gmail.com>
2026-01-26 12:20:53 +0530

    Time: 25 ms (8.74%) | Memory: 68.9 MB (5.67%) - LeetSync

--- 1306-minimum-absolute-difference/minimum-absolute-difference.java
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.java
@@ -5,6 +5,10 @@ class Solution {
 
         int absDiff = Integer.MAX_VALUE;
         int n = arr.length;
+
+        int[][] pairs = new int[n+1][2];
+        int count=0;
+
         List<List<Integer>>result = new ArrayList<>();
         for(int i=1;i<n;i++){
             absDiff = Math.min(absDiff,arr[i]-arr[i-1]);
@@ -12,8 +16,15 @@ class Solution {
 
         for(int i=1;i<n;i++){
             if(arr[i]-arr[i-1] == absDiff){
-                result.add(Arrays.asList(arr[i-1],arr[i]));
+                // result.add(Arrays.asList(arr[i-1],arr[i]));
+                pairs[count][0] = arr[i-1];
+                pairs[count][1] = arr[i];
+                count++;
+            
             }
+        }
+        for(int i=0;i<count;i++){
+            result.add(Arrays.asList(pairs[i][0],pairs[i][1]));
         }
         return result;
     }
