eb68cd7f2aa6cb1c46d8d12fd92c8848d09b1ca6
Deep Singnapure <deepsingnapure5@gmail.com>
2026-02-05 09:40:29 +0530

    Time: 1 ms (98.72%) | Memory: 46.9 MB (30.77%) - LeetSync

---
+++ 3651-transformed-array/transformed-array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] constructTransformedArray(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+
+        for(int i=0;i<n;i++){
+            int shift = nums[i]%n;
+            
+            int newIdx = (i + nums[i])%n;
+
+            if(newIdx < 0){
+                newIdx += n;
+            }
+            result[i] += nums[newIdx];
+        }
+        return result;
+    }
+}
